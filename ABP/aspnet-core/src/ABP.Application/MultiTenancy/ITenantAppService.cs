d9630c59e3ecd32c3b1e671abcd91043c6871540
Roberto Santana Perdomo <rsantanap@outlook.com>
2018-09-27 20:55:48 -0300

    Added ABP sample

---
+++ ABP/aspnet-core/src/ABP.Application/MultiTenancy/ITenantAppService.cs
@@ -0,0 +1,10 @@
+﻿using Abp.Application.Services;
+using Abp.Application.Services.Dto;
+using ABP.MultiTenancy.Dto;
+
+namespace ABP.MultiTenancy
+{
+    public interface ITenantAppService : IAsyncCrudAppService<TenantDto, int, PagedResultRequestDto, CreateTenantDto, TenantDto>
+    {
+    }
+}
