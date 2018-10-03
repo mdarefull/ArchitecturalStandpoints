8bd9d4fcfca33215a0f60c18671fda2641cef5e4
Roberto Santana Perdomo <rsantanap@outlook.com>
2018-10-03 11:33:33 -0300

    Playing with EF

---
+++ Commons.Repository.Abstracts/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+
+namespace Commons.Repository
+{
+    public interface IRepository<TModel>
+    {
+        TModel GetById(int id);
+
+        IQueryable<TModel> GetAll();
+
+        TModel Insert(TModel model);
+
+        TModel Update(TModel model);
+    }
+}
